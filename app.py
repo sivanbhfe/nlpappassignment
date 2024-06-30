6458b69e54dbc4eb51942ee2d7499aaed22becb6
Utkarsh Anant <uanant007@gmail.com>
2024-06-30 10:01:25 +0530

    refactor: :recycle: Update base code files.

--- app.py
+++ app.py
@@ -1,56 +1,47 @@
-# Using flask to make an api
-# import necessary libraries and functions
-from flask import Flask, jsonify, request, make_response, send_file, json, render_template
+from flask import Flask, request, send_file, render_template
 from flask_cors import CORS
 import networkx as nx
-from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+import matplotlib
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-# creating a Flask app
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+matplotlib.use("agg")
 
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/addrelationship", methods=["POST"])
 def addrelationship():
-    details = request.data
-    decoded = json.loads(details)
-    testing = eval(decoded)
+    details = request.get_json()  # Get the JSON data from the request
     G = nx.DiGraph()
-    for i in range(len(testing)):
-        G.add_node(testing[i]["fentity"], name=testing[i]["fentity"])
-        G.add_node(testing[i]["sentity"], name=testing[i]["sentity"])
-        G.add_edge(
-            testing[i]["fentity"],
-            testing[i]["sentity"],
-            start=testing[i]["fentity"],
-            end=testing[i]["fentity"],
-            relation=testing[i]["fentity"]
-            + " "
-            + testing[i]["relationship"]
-            + " "
-            + testing[i]["sentity"],
-        )
+
+    for item in details:
+        fentity = item["fentity"]
+        sentity = item["sentity"]
+        relationship = item["relationship"]
+
+        G.add_node(fentity, name=fentity)
+        G.add_node(sentity, name=sentity)
+        G.add_edge(fentity, sentity, relation=f"{fentity} {relationship} {sentity}")
+
     pos = nx.spring_layout(G)
-    nx.draw(G, pos)
-    node_labels = nx.get_node_attributes(G, "name")
-    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    nx.draw(G, pos, with_labels=True)
     edge_labels = nx.get_edge_attributes(G, "relation")
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-    # nx.draw(G,node_color='red',labels={node: node for node in G.nodes()})
-    img = BytesIO()  # file-like object for the image
-    plt.savefig(img)  # save the image to the stream
-    img.seek(0)  # writing moved the cursor to the end of the file, reset
-    plt.clf()  # clear pyplot
+
+    img = BytesIO()
+    plt.savefig(img, format="png")
+    img.seek(0)
+    plt.clf()
+
     return send_file(img, mimetype="image/png")
 
 
-# driver function
 if __name__ == "__main__":
     app.run(debug=True)
