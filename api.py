93d616e38099e6eb8e8f0a02f0c73ba941c10127
Sivabalan <sivansbhfe@gmail.com>
2024-06-30 19:25:18 +0530

    graph in company hierarchy view

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import networkx as nx
 from networkx.drawing.nx_agraph import write_dot, graphviz_layout
 import matplotlib.pyplot as plt
 from io import BytesIO
+import random
 
 # creating a Flask app
 app = Flask(__name__)
@@ -26,7 +27,8 @@ def addrelationship():
         G.add_node(testing[i]['sentity'],name=testing[i]['sentity'])
         # G.add_edge(testing[i]['fentity'], testing[i]['sentity'], start=testing[i]['fentity'], end=testing[i]['fentity'],relation=testing[i]['fentity']+' '+testing[i]['relationship']+' '+testing[i]['sentity'])
         G.add_edge(testing[i]['fentity'], testing[i]['sentity'])
-    pos = nx.spring_layout(G)
+    # pos = nx.spring_layout(G)
+    pos = hierarchy_pos(G)
     nx.draw(G,pos)
     node_labels = nx.get_node_attributes(G,'name')
     nx.draw_networkx_labels(G, pos, labels=node_labels)
@@ -38,6 +40,76 @@ def addrelationship():
     img.seek(0)  # writing moved the cursor to the end of the file, reset
     plt.clf()  # clear pyplot
     return send_file(img, mimetype='image/png')
+
+def hierarchy_pos(G, root=None, width=1, vert_gap=0.2, vert_loc=0, xcenter=0.5):
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723
+    Licensed under Creative Commons Attribution-Share Alike
+
+    If the graph is a tree this will return the positions to plot this in a
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+
+    root: the root node of current branch
+    - if the tree is directed and this is not given,
+      the root will be found and used
+    - if the tree is directed and this is given, then
+      the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given,
+      then a random choice will be used.
+
+    width: horizontal space allocated for this branch - avoids overlap with other branches
+
+    vert_gap: gap between levels of hierarchy
+
+    vert_loc: vertical location of root
+
+    xcenter: horizontal location of root
+    '''
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  # allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1, vert_gap=0.2, vert_loc=0, xcenter=0.5, pos=None, parent=None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root: (xcenter, vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)
+        if len(children) != 0:
+            print(len(children))
+            print("1st width",width)
+            width = 1
+            dx = width / len(children)
+            print("DX",dx)
+            nextx = xcenter - width / 2 - dx / 2
+            for child in children:
+                print("CHIDL", child)
+                nextx += dx
+                print("2nd WIDTH",width)
+                pos = _hierarchy_pos(G, child, width=dx, vert_gap=vert_gap,
+                                     vert_loc=vert_loc - vert_gap, xcenter=nextx,
+                                     pos=pos, parent=root)
+
+        return pos
+    print("3rd Width:",width)
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
 # driver function
 if __name__ == '__main__':
